8e5e67b1869c01d35b7e61113e73d763ee7dd227
name <chanmi5857@gmail.com>
2021-02-19 09:29:31 +0900

    완주하지못한선수

--- programmers/test3.py
+++ programmers/test3.py
@@ -11,3 +11,8 @@ def solution(participant, completion):
 participant = 	["marina", "josipa", "nikola", "vinko", "filipa"]
 completion = ["josipa", "filipa", "marina", "nikola"]
 print(solution(participant, completion))
+
+# import collections
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
